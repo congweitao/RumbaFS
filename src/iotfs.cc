dccfe93b1af062e4d0017b9170f1c9e12c1e68ac
weitao <congweitao@hotmail.comm>
2019-08-23 23:21:12 +0800

    new

--- src/iotfs.cc
+++ src/iotfs.cc
@@ -1,11 +1,1 @@
-#include <signal.h>  // signal()
-#include <unistd.h> // _exit()
 
-/* catch ctrl-c and destruct */
-void stop(int signo) {
-    _exit(0);
-}
-
-int main() {
-    signal(SIGINT, stop);
-}
